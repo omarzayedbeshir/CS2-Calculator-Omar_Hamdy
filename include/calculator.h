10f6dfb0b1cb43ca21e0061eda1bd193194f24bd
Hamdy410 <hamdyousama79@gmail.com>
2025-03-26 10:41:48 +0200

    Added calculator functions in calculator.h

--- include/calculator.h
+++ include/calculator.h
@@ -4,3 +4,9 @@ double add(double x, double y);
 double subtract(double x, double y);
 double multiply(double x, double y);
 double divide(double x, double y);
+int factorial(int x);
+int GCD(int x, int y);
+int LCM(int x, int y);
+int random(int start, int end);
+
+
